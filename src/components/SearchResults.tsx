995b7de7156df66925fc3c6e466d39f466ccba2b
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-07-04 03:15:25 -0300

    Otimizações

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,18 +1,32 @@
 import { ProductItem } from './ProductItem';
 
 interface SearchResultsProps {
+  totalPrice: number;
   results: Array<{
     id: number;
     price: number;
+    priceFormatted: string;
     title: string;
   }>;
+  onAddToWishlist: (id: number) => void;
 }
 
-export function SearchResults({ results }: SearchResultsProps) {
+export function SearchResults({
+  results,
+  totalPrice,
+  onAddToWishlist,
+}: SearchResultsProps) {
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map((product) => {
-        return <ProductItem key={product.id} product={product} />;
+        return (
+          <ProductItem
+            key={product.id}
+            product={product}
+            onAddToWishlist={onAddToWishlist}
+          />
+        );
       })}
     </div>
   );
